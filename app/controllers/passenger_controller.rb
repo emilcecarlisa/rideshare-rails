9bc7bec873bd701d39cd0d796982d5aeba52feff
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-03 16:24:17 -0700

    added in passenger_controller. set up method stubs.

---
+++ app/controllers/passenger_controller.rb
@@ -0,0 +1,24 @@
+class PassengerController < ApplicationController
+
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+end
