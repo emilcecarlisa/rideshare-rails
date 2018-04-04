b9d98b3e0b22d61a2b514c5c0716587b638a032b
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-03 17:53:03 -0700

    added in methods for passenger controller. stubbed out passenger views

--- app/controllers/passenger_controller.rb
+++ app/controllers/passenger_controller.rb
@@ -1,24 +1,50 @@
 class PassengerController < ApplicationController
 
   def new
+    @book = Passenger.new
   end
 
   def index
+    @passengers = Passenger.all
   end
 
   def create
+    @passenger = Passenger.new(passenger_params)
+
+    if @passenger.save
+      redirect_to '/passengers'
+    else
+      render :new
   end
 
   def update
+    updated_info = params[:passenger]
+    @passenger = Passenger.find(passenger_id)
+    @passenger.assign_attributes(passenger_params)
+
+    if @passenger.save
+      redirect_to passenger_path(passenger)
+    else
+      render :edit
+    end
   end
 
   def show
+    @passenger = Passenger.find(params[:id])
   end
 
   def edit
+    @passenger = Passenger.find(params[:id])
   end
 
   def destroy
+    Passenger.destroy(params[:id])
+    redirect_to passenger_path
   end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_number)
 
 end
