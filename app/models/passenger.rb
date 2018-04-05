bd9327dd4bc07cb4fa35314b589ad06edec6bb4d
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-04 23:13:01 -0700

    added links to show html; added method to passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,13 @@ class Passenger < ApplicationRecord
 
   has_many :trips
 
+  def self.total_cost
+    total = 0
+
+    Passenger.trips.each do |trip|
+      total += trip.cost
+    end
+    return total
+  end
+
 end
