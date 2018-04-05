1d2682b994778730815a5bf54a685609c93f9cb2
emilcecarlisa <emilcecarlisa@gmail.com>
2018-04-04 23:57:33 -0700

    Driver list displaying and total earnings on each driver page rendering

---
+++ app/controllers/drivers_controller.rb
@@ -0,0 +1,52 @@
+class DriversController < ApplicationController
+# if the driver is selected, show the total revenue , avg rating, --clicking on list will take you to trips page
+
+  def new
+    @driver = Driver.new
+  end
+
+  def index
+    @drivers = Driver.all
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+
+    if @driver.save
+      redirect_to drivers_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @driver = Driver.find(params[:id])
+  end
+
+  def edit
+    @driver = Driver.find(params[:id])
+  end
+
+  def update
+    @driver = Driver.find(params[:id])
+
+    @driver.assign_attributes(driver_params)
+
+    if @driver.save
+      redirect_to driver_path(driver)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+     Driver.destroy(params[:id])
+      redirect_to driver_path
+  end
+
+  private
+  def driver_params
+    return params.require(:driver).permit(:name, :driver_id, :vin)
+  end
+
+end
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,23 @@
 class Driver < ApplicationRecord
+  has_many :trips
 
-has_many :trips
+  FEE = 1.65
+  DRIVER_SHARE = 0.8
+
+  def total_revenue
+
+    subtotal = 0
+    trips.each do |trip|
+      subtotal += trip.cost - FEE
+    end
+
+    total = subtotal * DRIVER_SHARE
+    return total.to_f
+  end
+
+  # def avg_revenue
+  #   avg_revenue = (total_revenue/drivers.length)
+  #   return avg_revenue_per_hr = (avg_revenue/60).round(2)
+  # end
 
 end
